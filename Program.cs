d96585f3971744de710a92cc4c4bb85dbb807f49
Luiz Anunciacao <luiz.itdev@gmail.com>
2025-08-21 20:16:19 -0300

    DesafioPooCelularC#Luiz

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", "32GB");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\nSmartphone iPhone:");
+        Smartphone iphone = new Iphone("987654321", "iPhone 13", "987654321098765", "128GB");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Safari");
+    }
+}
